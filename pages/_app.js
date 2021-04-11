bd50284df159c61a0b7242db33d2e985faa5c033
CarlinCharge <sprintman210@gmail.com>
2021-04-11 11:40:48 -0400

    firebase setup with db connect

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import firebaseConfig from "./firebaseconfig";
+import { useEffect } from "react";
+
+
+
+if (!firebase.apps.length) {
+firebase.initializeApp(firebaseConfig);
+};
+
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect (() => {
+    const db = firebase.firestore()
+
+      db.collection("homework").doc("bARR2Gh7JceJxcJxFofc")
+    .onSnapshot((doc) => {
+        console.log("Current data: ", doc.data());
+    });
+  })
   return <Component {...pageProps} />
 }
 
---
+++ pages/firebaseconfig.js
@@ -0,0 +1,11 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyCF6Arjqdw8bN3hx6jInQ2ZnUC6soFCrhM",
+    authDomain: "homework-planner-44a90.firebaseapp.com",
+    projectId: "homework-planner-44a90",
+    storageBucket: "homework-planner-44a90.appspot.com",
+    messagingSenderId: "118957187068",
+    appId: "1:118957187068:web:1e10c21a7074f52c639c79",
+    measurementId: "G-DWTF28FLNK"
+  };
+
+  export default firebaseConfig;
